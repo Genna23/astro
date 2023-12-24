c319c19b8c0fff9a8d25da6dbd57b42ab0a3508e
Genna23 <mogilnikoff.gena@yandex.ru>
2023-12-24 23:01:50 +0300

    First init

---
+++ src/data/navData.js
@@ -0,0 +1,16 @@
+const data = [
+        {
+                name: 'Главная',
+                path: '/'
+        },
+        {
+                name: 'О нас',
+                path: '/about'
+        },
+        {
+                name: 'Контакты',
+                path: '/contacts'
+        }
+]
+
+export default data
