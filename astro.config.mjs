59d8b0754d3caf5fe66b11baf39b7af2062cc277
Genna23 <mogilnikoff.gena@yandex.ru>
2024-05-15 01:21:29 +0300

    add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import storyblok from '@storyblok/astro';
 import tailwind from "@astrojs/tailwind";
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import icon from "astro-icon";
-
 import { loadEnv } from "vite";
-const { PUBLIC_ENV, STORYBLOK_TOKEN_ACCESS} = loadEnv(import.meta.env.MODE, process.cwd(), "");
-
+import sitemap from "@astrojs/sitemap";
+const {
+  PUBLIC_ENV,
+  STORYBLOK_TOKEN_ACCESS
+} = loadEnv(import.meta.env.MODE, process.cwd(), "");
 const baseComponents = {
   page: 'components/Page',
   faqsComponent: 'components/Base/Faqs/Faqs',
@@ -14,19 +16,16 @@ const baseComponents = {
   config: 'components/Base/Config',
   seo: 'components/Base/Seo',
   richText: 'components/Base/RichText'
-}
-
+};
 const faqsComponents = {
   faqs: 'components/Faqs/Faqs',
   faqsCategory: 'components/Faqs/FaqsCategory',
   faqsItem: 'components/Faqs/FaqsItem'
-}
-
+};
 const blogComponents = {
   article: 'components/Blog/Article',
-  blog: 'components/Blog/Blog',
-}
-
+  blog: 'components/Blog/Blog'
+};
 const homeComponents = {
   hero: 'components/Home/Hero',
   about: 'components/Home/About',
@@ -36,33 +35,32 @@ const homeComponents = {
   process: 'components/Home/Process',
   processItem: 'components/Home/ProcessCard',
   stats: 'components/Home/Stats',
-  statsItem: 'components/Home/StatsItem',
-}
-
+  statsItem: 'components/Home/StatsItem'
+};
 const aboutComponents = {
   aboutHero: 'components/About/Hero',
   logoGrid: 'components/About/LogoList',
   steps: 'components/About/Steps',
-  history: 'components/About/History',
-}
-
+  history: 'components/About/History'
+};
 const portfolioComponents = {
   category: 'components/Portfolio/Category',
   portfolio: 'components/Portfolio/Portfolio',
-  portfolioItem: 'components/Portfolio/PortfolioItem',
-}
-
+  portfolioItem: 'components/Portfolio/PortfolioItem'
+};
 const deliveryComponents = {
   deliveryHero: 'components/Delivery/Hero',
   deliverySteps: 'components/Delivery/Steps',
   deliveryStepsItem: 'components/Delivery/StepsItem',
   deliveryMethods: 'components/Delivery/Methods',
   deliveryMethodsItem: 'components/Delivery/MethodsItem',
-  deliveryConditions: 'components/Delivery/Conditions',
-}
+  deliveryConditions: 'components/Delivery/Conditions'
+};
+
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://charm-wood.ru",
   integrations: [tailwind(), storyblok({
     accessToken: STORYBLOK_TOKEN_ACCESS,
     components: {
@@ -74,7 +72,9 @@ export default defineConfig({
       ...portfolioComponents,
       ...deliveryComponents
     }
-  }), icon()],
+  }), icon(), sitemap({
+    filter: (page) => page !== 'https://charm-wood.ru/portfolio' && page.includes('portfolio'),
+  })],
   vite: {
     plugins: [basicSsl()],
     server: {
